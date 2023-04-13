b4ac90fe2a7487c069f433d0565817325ce1f15c
vandart10 <73397775+vandart10@users.noreply.github.com>
2023-04-13 23:35:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ import scipy.stats as stats
 chat_id = 710820274 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    (_, pvalue) = stats.mannwhitneyu(x, y)
-    return pvalue < 0.1
+    a = stats.anderson_ksamp([x, y])
+    return a.pvalue < 0.1
